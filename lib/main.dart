a1a5b12789839a2e10a471e87285f4fb43f61c05
Khakachang Tripura <khakachang22@gmail.com>
2023-05-08 21:08:37 +0530

    Initilizing app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:take_note/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
