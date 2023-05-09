49045a2e22e28cfe125c017d388f56135afb1885
Khakachang Tripura <khakachang22@gmail.com>
2023-05-09 18:09:09 +0530

    Completed everything and the app works perfectly fine

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:take_note/screens/note_editor.dart';
+import 'package:take_note/screens/note_reader.dart';
 import 'package:take_note/style/app_style.dart';
 import 'package:take_note/widgets/note_card.dart';
 
@@ -54,7 +56,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2),
                       children: snapshot.data!.docs
-                          .map((note) => noteCard(() {}, note))
+                          .map((note) => noteCard(() {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          NoteReaderScreen(note),
+                                    ));
+                              }, note))
                           .toList(),
                     );
                   }
@@ -68,6 +77,14 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => NoteEditorScreen()));
+        },
+        label: Text("Add Note"),
+        icon: Icon(Icons.add),
       ),
     );
   }
