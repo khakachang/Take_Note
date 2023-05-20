81f3399c290d9edccd4e5644087fbfe4aa6f30e4
Khakachang Tripura <khakachang22@gmail.com>
2023-05-20 23:01:33 +0530

    delete botton added to note reader

--- lib/screens/note_reader.dart
+++ lib/screens/note_reader.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:take_note/style/app_style.dart';
 
 class NoteReaderScreen extends StatefulWidget {
-  NoteReaderScreen(this.doc, {super.key});
+  NoteReaderScreen(this.doc, {Key? key});
   QueryDocumentSnapshot doc;
+
   @override
   State<NoteReaderScreen> createState() => _NoteReaderScreenState();
 }
@@ -20,36 +21,32 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
         leading: BackButton(
           color: Colors.black,
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _showOptionsMenu(context);
+            },
+            icon: Icon(Icons.more_vert, color: Colors.black),
+          ),
+        ],
       ),
       body: Padding(
-        //Inside Note Padding
-        // padding: EdgeInsets.all(16.0),
-        padding: EdgeInsets.only(
-          left: 16,
-          right: 16,
-        ),
+        padding: EdgeInsets.only(left: 16, right: 16),
         child: SingleChildScrollView(
-          //Wrap in SingleChildScrollView To make the Column Scrollable
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 28.0,
-              ),
+              SizedBox(height: 28.0),
               Text(
                 widget.doc["note_title"],
                 style: AppStyle.mainTitle,
               ),
-              SizedBox(
-                height: 4.0,
-              ),
+              SizedBox(height: 4.0),
               Text(
                 widget.doc["creation_date"],
                 style: AppStyle.dateTitle,
               ),
-              SizedBox(
-                height: 28.0,
-              ),
+              SizedBox(height: 28.0),
               Text(
                 widget.doc["note_content"],
                 style: AppStyle.mainContent,
@@ -59,5 +56,54 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
         ),
       ),
     );
+  }
+
+  void _showOptionsMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Delete Note'),
+              onTap: () {
+                _deleteNote();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteNote() async {
+    try {
+      await FirebaseFirestore.instance
+          .collection("Notes")
+          .doc(widget.doc.id) // Use the document ID of the note
+          .delete();
+
+      // Show a snackbar or any other notification indicating successful deletion
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Note deleted successfully"),
+        ),
+      );
+
+      // Navigate back to the previous screen
+      Navigator.pop(context);
+    } catch (e) {
+      // Handle any errors that occurred during deletion
+      print('Failed to delete note: $e');
+      // Show a snackbar or any other notification indicating the error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Failed to delete note"),
+        ),
+      );
+    }
   }
 }
